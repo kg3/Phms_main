a4b843d6ece16fcf7bf47b7c2a14e4ff7264b1cb
kg3 <kg3@users.noreply.github.com>
2015-11-20 16:31:46 -0600

    NotePad update

--- app/src/main/java/mobi/idappthat/phms_main/Activities/NotePadActivity.java
+++ app/src/main/java/mobi/idappthat/phms_main/Activities/NotePadActivity.java
@@ -38,7 +38,7 @@ public class NotePadActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_notepad);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
