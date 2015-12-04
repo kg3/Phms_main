979584de474341df6ff26af827342fb6667a8cd9
RandalWilkerson <Randal.Wilkerson@gmail.com>
2015-12-03 22:31:38 -0600

    Added input box, drop down menu, and button to search activity. Got rid of floating button in search activity

--- app/src/main/java/phms/main/Activities/SearchActivity.java
+++ app/src/main/java/phms/main/Activities/SearchActivity.java
@@ -6,16 +6,26 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.View.OnClickListener;
+
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
 
 import phms.main.R;
 
-public class SearchActivity extends AppCompatActivity {
+public class SearchActivity extends AppCompatActivity  {
 
     /*
         - search term
         - category
         - type {local, internet} // we don't need to get to internet if it's too much
      */
+    EditText etSearch;
+    TextView tList;
+    Button searchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +34,16 @@ public class SearchActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        etSearch = (EditText) findViewById(R.id.etSearch);
+
+        searchButton = (Button) findViewById(R.id.searchButton);
+
+
+        Spinner dropdown = (Spinner)findViewById(R.id.spinner1);
+        String[] items = new String[]{"All", "Diet", "Medicine", "Notes"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        dropdown.setAdapter(adapter);
+        /*
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,6 +52,6 @@ public class SearchActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        */
     }
-
 }
