b3e565443081da65cb275b384172114596beadf2
kg3 <kg3@users.noreply.github.com>
2015-12-05 14:03:21 -0600

    Implemented Parse ListAdapter; Everywhere you see /* !! PARSE !! */ that's a fresh edit you'll need to make; Also for any search you'll need to creat another noteAdapter.java in Models

--- app/src/main/java/phms/main/Activities/NotePadActivity.java
+++ app/src/main/java/phms/main/Activities/NotePadActivity.java
@@ -6,6 +6,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -14,11 +16,14 @@ import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
+import com.parse.ParseQueryAdapter;
 import com.parse.ParseUser;
 
 import java.util.List;
 
+import phms.main.Models.noteAdapter;
 import phms.main.R;
+
 
 /**
  *
@@ -28,11 +33,14 @@ public class NotePadActivity extends AppCompatActivity {
     public static final int CODE_NEW_NOTE = 0;
     public static final int ACTION_CANCEL = 0;
     public static final int ACTION_CREATE = 1;
-
-
     /* Declare Android Layouts */
     TextView tvNotes;
+    // list view
     ListView lvListView;
+    /* !! PARSE !! */
+    private ParseQueryAdapter<ParseObject> mainAdapter;
+    private noteAdapter noteAdapter;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,13 +51,44 @@ public class NotePadActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         /* Instantiate Android Layouts */
-        tvNotes = (TextView) findViewById(R.id.tvNotes);
-        lvListView = (ListView) findViewById(R.id.lvListView);
+        //tvNotes = (TextView) findViewById(R.id.tvNotes);          /* !! PARSE !! */
+        lvListView = (ListView) findViewById(R.id.list);
 
         /* load from database */
-        loadFromParse();
+        //loadFromParse();
 
-        /* Button for Note */
+        // /* !! PARSE !! */ Initialize main ParseQueryAdapter
+        mainAdapter = new ParseQueryAdapter<ParseObject>(this, "note");
+        mainAdapter.setTextKey("title");
+        //mainAdapter.setImageKey("image");
+
+        noteAdapter = new noteAdapter(this);
+
+        // /* !! PARSE !! */ Initialize ListView and set initial view to mainAdapter
+        listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(mainAdapter);
+        mainAdapter.loadObjects();
+
+        // /* !! PARSE !! */ Initialize toggle button
+        Button toggleButton = (Button) findViewById(R.id.toggleButton);
+        toggleButton.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if (listView.getAdapter() == mainAdapter) {
+                    listView.setAdapter(noteAdapter);
+                    noteAdapter.loadObjects();
+                } else {
+                    listView.setAdapter(mainAdapter);
+                    mainAdapter.loadObjects();
+                }
+            }
+
+        });
+
+
+
+        /* Button for Making a Note */
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,7 +104,6 @@ public class NotePadActivity extends AppCompatActivity {
 
     private void loadFromParse() {
 
-        /* query the database */
         ParseQuery<ParseObject> query = ParseQuery.getQuery("note");
         query.whereEqualTo("author", ParseUser.getCurrentUser());
 
@@ -74,10 +112,6 @@ public class NotePadActivity extends AppCompatActivity {
 
         query.findInBackground(new FindCallback<ParseObject>() {
             public void done(List<ParseObject> allNotes, ParseException e) {
-                // commentList now has the comments for myPost
-
-                // ArrayList noteArray = new ArrayList( allNotes.toArray() );
-                //lvListView.setAdapter();
 
                 if (!allNotes.isEmpty()) {
                     tvNotes.setText("");
@@ -110,6 +144,5 @@ public class NotePadActivity extends AppCompatActivity {
             }
         }
     }
-
 
 }
